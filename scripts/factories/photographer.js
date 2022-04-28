78c4d35a09b9602fae57237864b20dcfd03706e5
Lola Met <lola.met@free.fr>
2022-04-28 23:00:54 +0200

    Add photographers link and style index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,12 +6,17 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        img.classList.add('profile');
         img.setAttribute("src", picture)
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const photographerLink = document.createElement ('a');
+        photographerLink.href = "photographer.html?id="+id;
+        article.appendChild(photographerLink);
+        photographerLink.appendChild(img);
+        photographerLink.appendChild(h2);
+        
         
         const p = document.createElement ('p');
         const locationSpan = document.createElement ('span');
@@ -31,6 +36,9 @@ function photographerFactory(data) {
         p.appendChild(priceSpan);
 
         article.appendChild(p); 
+
+
+
 
         return (article);
     }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,18 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    const data = fetch("data/photographers.json")
+    .then(photographers => photographers.json())
+    return data;
+}
+
+async function init() {
+
+    // Récupère les datas des photographes
+    const params = (new URL(document.location)).searchParams;
+    const id = params.get("id");
+    const { photographers } = await getPhotographers();
+    const photographer = photographers.find(photographer => photographer.id.toString() === id.toString());
+    console.log(photographer);
+};
+
+init();
